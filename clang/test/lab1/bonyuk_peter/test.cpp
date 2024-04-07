0085d004fd1d9a90f52133eb44021eed2117a88c
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-04-07 14:06:37 +0300

    fdsf

---
+++ clang/test/lab1/bonyuk_peter/test.cpp
@@ -0,0 +1,19 @@
+// RUN: %clang_cc1 -load %llvmshlibdir/depWarningPluginBonyuk%pluginext plugin deprecated-warning %s 2>&1 | FileCheck %s
+
+// CHECK: warning: The 'deprecated' is in the function name
+void deprecated();
+
+// CHECK: warning: The 'deprecated' is in the function name
+void deprecatedasd();
+
+// CHECK: warning: The 'deprecated' is in the function name
+void deprecatedasfd();
+
+// CHECK-NOT: warning: The 'deprecated' is in the function name
+void something();
+
+// CHECK-NOT: warning: The 'deprecated' is in the function name
+void deprecatend();
+
+// CHECK-NOT: warning: The 'deprecated' is in the function name
+void deprecateqwe();
