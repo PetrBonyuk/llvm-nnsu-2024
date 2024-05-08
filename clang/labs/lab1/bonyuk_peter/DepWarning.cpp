351475382506bb5313157ef1b947e1d9d5e1c9f1
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-05-08 17:07:34 +0300

    lab1

--- clang/labs/lab1/bonyuk_peter/DepWarning.cpp
+++ clang/labs/lab1/bonyuk_peter/DepWarning.cpp
@@ -29,10 +29,10 @@ public:
     if (CaseInsensitive) {
       std::transform(FuncName.begin(), FuncName.end(), FuncName.begin(),
                      [](unsigned char c {
-        return std::tolower(c); });
+        return std::tolower(c); }));
       std::transform(Deprecated.begin(), Deprecated.end(), Deprecated.begin(),
                      [](unsigned char c {
-        return std::tolower(c); });
+        return std::tolower(c); }));
     }
 
     if (FuncName.find(Deprecated) != std::string::npos) {
