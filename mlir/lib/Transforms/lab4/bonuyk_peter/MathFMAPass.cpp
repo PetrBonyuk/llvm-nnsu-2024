29cdc99530d330806590c2c42f1daf2cbf45ab46
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-05-19 16:37:49 +0300

    lab4

--- mlir/lib/Transforms/lab4/bonuyk_peter/MathFMAPass.cpp
+++ mlir/lib/Transforms/lab4/bonuyk_peter/MathFMAPass.cpp
@@ -1,77 +1,73 @@
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
-#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
-#include "mlir/Support/LLVM.h"
-#include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Transforms/Utils.h"
+#include "mlir/Tools/Plugins/PassPlugin.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
 
 using namespace mlir;
+using namespace mlir::StandardOps;
 
 namespace {
-	class FusedMultiplyAddPass
-		: public PassWrapper<FusedMultiplyAddPass, OperationPass<ModuleOp>> {
-	public:
-		StringRef getArgument() const final { return "bonyuk_fused_multiply_add"; }
-		StringRef getDescription() const final {
-			return "This Pass combines the operations of addition and multiplication into one";
-		}
+class FusedMultiplyAddPass
+    : public PassWrapper<FusedMultiplyAddPass, OperationPass<ModuleOp>> {
+public:
+  StringRef getArgument() const final { return "bonyuk_fused_multiply_add"; }
+  StringRef getDescription() const final {
+    return "This Pass combines the operations of addition and multiplication into one";
+  }
 
-		void runOnOperation() override {
-			ModuleOp module = getOperation();
-			OwningRewritePatternList patterns(&getContext());
-			patterns.insert<FusedMultiplyAddPattern>(&getContext());
-			(void)applyPatternsAndFoldGreedily(module, std::move(patterns));
-		}
-	};
+  void runOnOperation() override {
+	  ModuleOp module = getOperation();
+	  module.walk([&](Operation *operation) {
+		  if (auto addOperation = dyn_cast<AddFOp>(operation)) {
+			  Value addLeft = addOperation.getOperand(0);
+			  Value addRight = addOperation.getOperand(1);
 
-	class FusedMultiplyAddPattern : public OpRewritePattern<AddFOp> {
-	public:
-		using OpRewritePattern<AddFOp>::OpRewritePattern;
+			  if (auto multiplyLeft = addLeft.getDefiningOp<MulFOp>()) {
+				  handleMultiplyOperation(addOperation, multiplyLeft, addRight);
+			  }
+			  else if (auto multiplyRight = addRight.getDefiningOp<MulFOp>()) {
+				  handleMultiplyOperation(addOperation, multiplyRight, addLeft);
+			  }
+		  }
+	  });
 
-		LogicalResult matchAndRewrite(AddFOp addOp,
-			PatternRewriter &rewriter) const override {
-			Value left = addOp.getOperand(0), right = addOp.getOperand(1);
-			Value lmul, rmul;
+	  module.walk([&](Operation *operation) {
+		  if (auto multiplyOperation = dyn_cast<MulFOp>(operation)) {
+			  if (multiplyOperation.use_empty()) {
+				  multiplyOperation.erase();
+			  }
+		  }
+	  });
+  }
 
-			if (matchPattern(left, m_Op<MulFOp>(m_Value(lmul), m_Value()))) {
-				replaceFMA(addOp, rewriter, lmul, right);
-			}
-			else if (matchPattern(right, m_Op<MulFOp>(m_Value(rmul), m_Value()))) {
-				replaceFMA(addOp, rewriter, rmul, left);
-			}
-			else {
-				return failure();
-			}
-			return success();
-		}
+private:
+  void HandMultiplyOperation(LLVM::FAddOp &AddOperation, LLVM::FMulOp &MultiplyOperation,
+                             Value &Operand) {
+    OpBuilder builder(AddOperation);
+    Value FMAOperation = builder.create<LLVM::FMAOp>(AddOperation.getLoc(), MultiplyOperation.getOperand(0),
+      MultiplyOperation.getOperand(1), Operand);
+    AddOperation.replaceAllUsesWith(FMAOperation);
 
-	private:
-		void replaceFMA(AddFOp addOp, PatternRewriter &rewriter, Value mulOp,
-			Value otherOperand) const {
-			Value fmaOp = rewriter.create<FMAFOp>(addOp.getLoc(), mulOp, otherOperand,
-				addOp.getResult());
-			rewriter.replaceOp(addOp, fmaOp);
-			if (mulOp.use_empty())
-				rewriter.eraseOp(mulOp);
-		}
-	};
+    if (MultiplyOperation.use_empty()) {
+      MultiplyOperation.erase();
+    }
+
+    if (FMAOperation.use_empty()) {
+      AddOperation.erase();
+    }
+  }
+};
 } // namespace
 
-void populateFusedMultiplyAddPatterns(RewritePatternSet &patterns) {
-	patterns.add<FusedMultiplyAddPattern>(&getContext());
-}
+MLIR_DECLARE_EXPLICIT_TYPE_ID(FusedMultiplyAddPass)
+MLIR_DEFINE_EXPLICIT_TYPE_ID(FusedMultiplyAddPass)
 
-void registerPasses() {
-	PassRegistration<FusedMultiplyAddPass>();
-	RewritePatternSet patterns(&getContext());
-	populateFusedMultiplyAddPatterns(patterns);
-	(void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
+PassPluginLibraryInfo getFusedMultiplyAddPassPluginInfo() {
+  return {MLIR_PLUGIN_API_VERSION, "bonyuk_fused_multiply_add", LLVM_VERSION_STRING,
+          []() { PassRegistration<FusedMultiplyAddPass>(); }};
 }
-
-} // namespace mlir
 
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
-	return { MLIR_PLUGIN_API_VERSION, "bonyuk_fused_multiply_add", LLVM_VERSION_STRING,
-			registerPasses };
+  return getFusedMultiplyAddPassPluginInfo();
 }
