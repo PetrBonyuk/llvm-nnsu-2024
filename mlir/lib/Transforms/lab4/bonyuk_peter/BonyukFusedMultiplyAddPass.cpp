14e480b829ce4054660180a0005bec37a56f0d81
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-05-31 08:54:35 +0300

    update

--- mlir/lib/Transforms/lab4/bonyuk_peter/BonyukFusedMultiplyAddPass.cpp
+++ mlir/lib/Transforms/lab4/bonyuk_peter/BonyukFusedMultiplyAddPass.cpp
@@ -7,7 +7,7 @@ using namespace mlir;
 
 namespace {
 class BonyukFusedMultiplyAddPass
-    : public PassWrapper<BonyukFusedMultiplyAddPass, 
+    : public PassWrapper<BonyukFusedMultiplyAddPass,
                          OperationPass<LLVM::LLVMFuncOp>> {
 public:
   StringRef getArgument() const final { return "bonyuk_fused_multiply_add"; }
@@ -18,7 +18,7 @@ public:
 
   void runOnOperation() override {
     LLVM::LLVMFuncOp func = getOperation();
-	func.walk([&](LLVM::FAddOp AddOperation) {
+    func.walk([&](LLVM::FAddOp AddOperation) {
       Value AddLeft = AddOperation.getOperand(0);
       Value AddRight = AddOperation.getOperand(1);
 
@@ -29,7 +29,7 @@ public:
       }
     });
 
-	func.walk([](LLVM::FMulOp MultiplyOperation) {
+    func.walk([](LLVM::FMulOp MultiplyOperation) {
       if (MultiplyOperation.use_empty()) {
         MultiplyOperation.erase();
       }
